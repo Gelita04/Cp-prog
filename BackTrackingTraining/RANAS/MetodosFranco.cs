d2809765b4b87571a3b5175bdf29bc53e8499fa2
JackRipper01 <francohernandezpiloto@gmail.com>
2022-12-27 19:27:30 -0500

    RANAS FINISHED

--- BackTrackingTraining/RANAS/MetodosFranco.cs
+++ BackTrackingTraining/RANAS/MetodosFranco.cs
@@ -1,15 +1,89 @@
-
+using System;
 
 namespace MetodosFranco
 {
-    static class Charco
+    public static class Charco
     {
+
         public static int ComiendoChocolates(bool[,] chocos, int[] ranas)
         {
-            for (int fila = 0; fila < chocos.GetLength(0); fila++)
+            int chocolatesComidos = 0;
+            int result = 0;
+            ComiendoChocolates(chocos, ranas, ref chocolatesComidos, ref result, 0, 0);
+            return result;
+            throw new Exception();
+        }
+        static void ComiendoChocolates(bool[,] chocos, int[] actualCoordFrogs, ref int chocolatesComidos, ref int result, int row, int ranaActual)
+        {
+            if (ranaActual == 0)
             {
+                for (int i = 0; i < actualCoordFrogs.Length; i++)
+                {
+                    if (chocos[row, actualCoordFrogs[i]])//si hay chocolate
+                        chocolatesComidos++;
+                }
+            }
+
+            if (row + 1 >= chocos.GetLength(0))//llegaste final
+            {
+                if (chocolatesComidos > result)
+                {
+                    int temp = chocolatesComidos;
+                    result = temp;
+                }
+                chocolatesComidos = 0;
+                return;
+            }
+
+
+            if (actualCoordFrogs[ranaActual] - 1 < 0)//no izq  coordFrogs[row][ranaActual]<= fue lo q cambie
+            {
+                if (ContainsRepitedElements(actualCoordFrogs))
+                {
+                    return;
+                }
+
+                ComiendoChocolates(chocos, actualCoordFrogs, ref chocolatesComidos, ref result, ranaActual + 1 >= actualCoordFrogs.Length ? row + 1 : row, ranaActual + 1 >= actualCoordFrogs.Length ? 0 : ranaActual + 1);//arreglar array
 
             }
+            else
+            {
+                actualCoordFrogs[ranaActual] -= 1;
+                if (ContainsRepitedElements(actualCoordFrogs))
+                {
+                    actualCoordFrogs[ranaActual] += 1;
+                    return;
+                }
+
+
+                ComiendoChocolates(chocos, actualCoordFrogs, ref chocolatesComidos, ref result, ranaActual + 1 >= actualCoordFrogs.Length ? row + 1 : row, ranaActual + 1 >= actualCoordFrogs.Length ? 0 : ranaActual + 1);//arreglar array
+
+            }
+            if (actualCoordFrogs[ranaActual] + 1 >= chocos.GetLength(1))//no derecha
+            { return; }
+
+            actualCoordFrogs[ranaActual] += 1;
+            if (ContainsRepitedElements(actualCoordFrogs))
+            {
+                actualCoordFrogs[ranaActual] -= 1;
+                return;
+            }
+
+            ComiendoChocolates(chocos, actualCoordFrogs, ref chocolatesComidos, ref result, ranaActual + 1 >= actualCoordFrogs.Length ? row + 1 : row, ranaActual + 1 >= actualCoordFrogs.Length ? 0 : ranaActual + 1);//arreglar array
+
+            return;
+        }
+        private static bool ContainsRepitedElements(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                        return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Equisde;
+using System.Diagnostics;
+
+using MetodosFranco;
+
+// BinaryTree<int> arbol =
+// new BinaryTree<int>(1,
+//     new BinaryTree<int>(2,
+//         new BinaryTree<int>(5),
+//         new BinaryTree<int>(4)),
+//     new BinaryTree<int>(8,
+//         new BinaryTree<int>(7),
+//         new BinaryTree<int>(10, null
+//             ,
+//             new BinaryTree<int>(11))));
+
+//System.Console.WriteLine(arbol.Inversions());
+
+
+//Cambia aca abajo el nombre por el del cs para cambiar los metodos.....o descomenta y comenta
+
+//using MetodosMauri;
+
+//implementa la solucion en MetodosFranco.cs - Todos los metodos
+//deben ser estaticos(hice la clase estatica, notese q no se instancia nunca)
+
+// System.Console.WriteLine("started");
+// var start = DateTime.Now.Ticks;
+
+// DESCOMENTA ESTO CUANDO SIENTAS Q TODO TA LINDO
+// int resultado1 = Charco.ComiendoChocolates(new bool[,] {
+// { false, false, false, false, false, false, false, false },
+// { false, false, true, false, false, true, false, false },
+// { false, true, false, false, true, false, true, false },
+// { false, false, true, false, false, false, false, true },
+// { false, false, false, false, false, false, false, false }
+// }, new int[] { 1, 3, 6 });
+
+// int resultado2 = Charco.ComiendoChocolates(new bool[,] {
+//             //           //                   //
+//  { false, false, false, false, false, false, false, false },
+//  { false, false, true, false, false, true, false, false },
+//  { false, true, false, false, true, false, true, false },
+//  { true, false, true, false, false, false, false, true }
+//  }, new int[] { 1, 3, 6 });
+
+// int resultado3 = Charco.ComiendoChocolates(new bool[,] {
+//  { false, false, false, false, false, false, false, false },
+//  { false, false, false, false, false, true, false, true },
+//  { false, false, false, false, false, false, true, false },
+//  { false, false, false, false, false, false, false, true }
+//  }, new int[] { 1, 3 });
+
+// double time = (double)(DateTime.Now.Ticks - start) / 1000000;
+// System.Console.WriteLine($"ended in {time} secs");
+
+// //Para cuando descomentes el 1 arriba
+// Console.WriteLine(resultado1);
+// Debug.Assert(resultado1 == 7);
+// System.Console.WriteLine($"OK");
+
+// Console.WriteLine(resultado2);
+// Debug.Assert(resultado2 == 7);
+// System.Console.WriteLine($"OK");
+
+// Console.WriteLine(resultado3);
+// Debug.Assert(resultado3 == 0);
+// System.Console.WriteLine($"OK");
+
+
+System.Console.WriteLine("started");
+var start = DateTime.Now.Ticks;
